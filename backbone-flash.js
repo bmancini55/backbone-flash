c6870ecda9f95b3357401fe9e9893fcb5386f403
bmancini55 <bmancini@gmail.com>
2013-10-20 14:59:18 -0700

    Add initial commit and bower package

---
+++ backbone-flash.js
@@ -0,0 +1,64 @@
+Backbone.Flash = {}
+_.extend(Backbone.Flash, {
+  initialize : function(config) {
+    this.config = _.extend({ 
+      el : 'body',
+      template: '',
+      stayDuration: 15000
+    }, config);
+    Backbone.on('flash', this.handleFlash, this);
+  },
+  handleFlash : function(flashParams, opts) {  
+    var tempConfig = _.extend({}, this.config, opts),
+        model = new this.flashModel(flashParams, { config: tempConfig })
+        view = new this.flashView({ model: model, config: tempConfig });
+    $(tempConfig.el).append(view.render().el);
+  },
+  flashModel : Backbone.Model.extend({
+    initialize: function(attr, options) {    
+      this.config = options.config
+
+      if(!this.get('persist'))
+        this.setTimer();
+    },  
+    defaults: {
+      persist: false,
+      type: 'success',
+      message: '',   
+    },
+    setTimer: function() {
+      var me = this;
+      this.set('timeoutHandle', window.setTimeout(function() { me.destroy() }, this.config.stayDuration));
+    },
+    clearTimeout: function() {
+      if(this.get('timeoutHandle'))
+        window.clearTimeout(this.get('timeoutHandle'));
+    }
+  }),
+  flashView : Backbone.View.extend({
+    tagName: 'div',
+    className: 'flash',
+    initialize: function(options) {      
+      this.config = options.config;
+      this.listenTo(this.model, 'destroy', this.remove, this);
+    },
+    events: {
+     'click button': 'close'
+    },
+    render: function() {            
+      this.$el.append(this.config.template(this.model.attributes));
+      return this;
+    },
+    close: function() {
+      this.model.clearTimeout();
+      this.model.destroy();   
+    },
+    remove: function() {
+      var me = this;
+      this.stopListening();
+      this.$el.hide("slow", function() { me.$el.remove() });    
+    }
+  })
+});
+
+
